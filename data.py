40bdee84ba6d83e40a1dfd6e016cb42232c1eca7
Naman Sharma <namansharma232009@gmail.com>
2022-06-07 16:51:14 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,23 @@
+import os
+from GoogleImageScrapper import GoogleImageScraper
+from patch import webdriver_executable
+
+if __name__ == "__main__":
+    #Define file path
+    webdriver_path = os.path.normpath(os.path.join(os.getcwd(), 'webdriver', webdriver_executable()))
+    image_path = os.path.normpath(os.path.join(os.getcwd(), 'photos'))
+
+    #Add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
+    search_keys= ['apple','t-shirt']
+
+    #Parameters
+    number_of_images = 50
+    headless = False
+    min_resolution=(0,0)
+    max_resolution=(9999,9999)
+
+    #Main program
+    for search_key in search_keys:
+        image_scrapper = GoogleImageScraper(webdriver_path,image_path,search_key,number_of_images,headless,min_resolution,max_resolution)
+        image_urls = image_scrapper.find_image_urls()
+        image_scrapper.save_images(image_urls)
